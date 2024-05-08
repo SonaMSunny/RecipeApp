b1473b4287906fccc535e6523c717a50a4b3f279
SonaMSunny <sonamsunnykuttiplavil@gmail.com>
2024-05-08 22:22:33 +0530

    recipeapp

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import RecipeForm from './component/RecipeForm.jsx';
+
+function App() {
+  return (
+    <div>
+      <RecipeForm/>
+    </div>
+  );
+}
+
+export default App;
